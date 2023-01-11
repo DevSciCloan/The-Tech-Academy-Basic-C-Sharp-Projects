ec6856b31bd2e42f06bd2953e93d423dbb48ea08
DevSciCloan <116118420+DevSciCloan@users.noreply.github.com>
2023-01-11 12:33:10 -0700

    Uploading carInsurance

---
+++ carInsurance/carInsurance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace carInsurance
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Instantiating required variables
+            bool oldEnough;
+            bool hasDUI;
+            int tickets;
+
+            // Collecting client information
+            Console.WriteLine("What is your age?");
+            oldEnough = Convert.ToInt32(Console.ReadLine()) > 15; // Getting user age and converting string input to int before testing if the result is > 15. Then storing result as bool in oldEnough
+            Console.WriteLine("Have you ever had a DUI? (\"yes\" or \"no\")");
+            hasDUI = Console.ReadLine() == "yes" ? true : false; // Getting user input as string and converting to bool using ternary operator
+            Console.WriteLine("How many speeding tickets do you have?");
+            tickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Client is eligible: \n" + (oldEnough && hasDUI && tickets <= 3)); // Are all three conditions in parentheses met? If True print true else false
+
+            Console.Read(); // Waiting for user input before closing program
+        }
+    }
+}

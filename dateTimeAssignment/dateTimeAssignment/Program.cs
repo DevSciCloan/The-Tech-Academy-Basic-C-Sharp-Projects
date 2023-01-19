0f3a0204291568e483aae569366ee36d09e85306
DevSciCloan <116118420+DevSciCloan@users.noreply.github.com>
2023-01-19 12:58:27 -0700

    Uploading dateTimeAssignment

---
+++ dateTimeAssignment/dateTimeAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace dateTimeAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(DateTime.Now.ToString("f")); // Writes the current date and time to the console in standard date and time format f
+
+            Console.WriteLine("Please enter a number: \n"); // Prompts user to enter a number
+
+            // Gets current date and time from user's computer,
+            // reads user input and converts to integer,
+            // passes the integer into AddHours() and formats the returned DateTime object to format f using ToString,
+            // Writes the final formatted string to the console
+            Console.WriteLine((DateTime.Now.AddHours(Convert.ToInt32(Console.ReadLine()))).ToString("f")); 
+
+            Console.Read(); // Waits for user input before closing the program
+        }
+    }
+}

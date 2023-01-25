b22e37d5bf550d12d30c74f36b0eb8460aade31b
DevSciCloan <116118420+DevSciCloan@users.noreply.github.com>
2023-01-25 12:16:42 -0700

    Uploading Challenge2

---
+++ Challenge2/Challenge2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var student = new Student() { FirstName = "Jonathan", LastName = "Mangone" };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+        }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
